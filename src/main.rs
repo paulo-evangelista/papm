683b7bce3bd06f98aec6142fb828407e11016fd6
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-04-25 11:41:43 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand};
-use std::env;
+use std::{env, fs, io::{self, Read, Write}, panic::catch_unwind};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -21,7 +21,11 @@ enum Commands {
         #[arg(help = "The name of the password to retrieve")]
         name: String,
     },
-    // Placeholder for the 'set' command
+    #[command(about = "Create a new password storage in the given file, encrypted with the provided password", long_about = None)]
+    CreateStorage {
+        #[arg(help = "The file path" , default_value_t = String::from("~/.papm"))]
+        file: String,
+    },
     Set {
         // Arguments for 'set' command
     },
@@ -33,7 +37,7 @@ fn main() {
     let password = cli.password.or_else(|| env::var("PAPM_PASSWORD").ok());
 
     if password.is_none() {
-        eprintln!("No password provided");
+        eprintln!("-> No password provided!\n   You can provide the password using the '--password' flag or the 'PAPM_PASSWORD' environment variable.");
         // Exit the program
         std::process::exit(1);
     }
@@ -46,5 +50,39 @@ fn main() {
         Commands::Set { .. } => {
             // Implementation for 'set' command
         },
+        Commands::CreateStorage {file} => {
+            let canonical_path = fs::canonicalize(file).unwrap_or_else(|_| {
+                eprintln!("-> Error getting the canonical path for: {}", file);
+                std::process::exit(1);
+            });
+
+            let file_contents = fs::read(canonical_path).unwrap_or_else(|_| {
+                eprintln!("-> Error reading file: {}", file);
+                std::process::exit(1);
+            });
+
+            if !file_header.is_empty() {
+                print!("-> Attention! The file is not empty. Do you want to overwrite it? (y/N): ");
+                std::io::stdout().flush().unwrap();
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).unwrap();
+                if input.trim().to_lowercase() == "y" {
+                    println!("-> ok, overwriting...");
+                } else {
+                    println!("-> Cancelled.");
+                    std::process::exit(0);
+                }
+            }
+        },
+    }
+}
+
+fn read_first_10_bytes(file_path: &str) -> Option<String> {
+    let mut file = fs::File::open(file_path);
+    let mut buffer = vec![0; 20]; // Cria um buffer para 10 bytes
+    fs::File::read_exact(&mut buffer).is_err(); // Lê exatamente 10 bytes
+    match String::from_utf8(buffer) {
+        Ok(s) => Some(s),
+        Err(_) => None
     }
 }
